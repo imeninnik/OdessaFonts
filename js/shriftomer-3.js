7795fefc8242f3dd94be78c34579aca552f7f177
Pavel Karlovich <p.karlovich@gmail.com>
2016-03-21 21:53:09 +0200

    clean up

--- js/shriftomer-3.js
+++ js/shriftomer-3.js
@@ -1,121 +1,105 @@
 (function ($) {
     'use strict';
+    $(document).ready(run);
 
-    var currentFontData;
-
-    var fonts = {
-        Arial: {k:4, m:56.6},
-        ARIAL: {k:5, m:66.6},
-        Direct: {k:4.8780487804878, m:51.7},
-        DIRECT: {k:7, m:61.7},
-        Meringue: {k:5, m:76.6},
-        MERINGUE: {k:5, m:76.6},
-        Tahoma: {k:6, m:86.6},
-        TAHOMA: {k:6, m:86.6},
-        Times: {k:8, m:16.6},
-        TIMES: {k:8, m:16.6},
-        Verdana: {k:9, m:26.6},
-        VERDANA: {k:9, m:26.6}
-    };
-
-    var defaultFontData =  {k:4.8780487804878, m:51.7};
-
-    var dotSize = 0.0320855526772571;
-
-    var $fontFamilyInput = $('#font-family-input');
-    var $lettersCaseSelect = $('#letters-case-select');
-    var $distanceInput =  $('#distance-input');
-    var $letterBodySizeInput = $('#letter-body-size-input');
-
-    var $distanceOutput = $('#distance-output');
-    var $lettersHeightOutput = $('#letters-height-output');
-    var $lettersBodySizeOutput = $('#letters-body-size-output');
-    var $symbolsHeight = $('#symbols-height');
+    function run() {
+        var currentFontData, defaultFontData,
+            bypassCalculationForDistance, bypassCalculationForHeight,
+            currentDistance, currentHeight, mapInitInterval, map;
 
 
 
-    var $map = $('.js-map');
-    $('#show-on-map').click(function() {
-        var visible = ($('.mapus').css('visibility') == 'visible');
-        visible ? $('.mapus').css('visibility','hidden') : $('.mapus').css('visibility','visible');
-    });
+        var $distanceInput =  $('#distance-input');
+        var $symbolsHeight = $('#symbols-height');
+        var $map = $('.mapus');
+
+        var fonts = {
+            //original Direct type
+            DIRECT: {k:4.8780487804878, m:51.7, ratio: 2.732 }
+        };
+
+        defaultFontData = fonts.DIRECT;
+        currentFontData = defaultFontData;
+
+        mapInitInterval = setInterval(function() {
+            if (window.ymaps && window.ymaps.Map) {
+                initMap();
+                clearInterval(mapInitInterval);
+                mapInitInterval = null;
+            }
+        },300);
 
 
-    $([$fontFamilyInput, $lettersCaseSelect]).each(function(){
-        this.on('change focus blur', recalculateAll);
-    });
 
-    $($distanceInput).on('change focus blur', calculateRequiredFontBodySize);
-    //$($letterBodySizeInput).on('change focus blur', calculateDistance);
-    $($symbolsHeight).on('change focus blur', calculateDistance);
-
-    recalculateAll();
+        $('#show-on-map').click(function() {
+            var visible = ($map.css('visibility') == 'visible');
+            visible ? $map.css('visibility','hidden') : $map.css('visibility','visible');
+        });
 
 
-    function recalculateAll() {
+        $($distanceInput).on('focus', function() { currentDistance = $($distanceInput).val(); });
+        $($symbolsHeight).on('focus', function() { currentHeight = $($symbolsHeight).val();  });
 
-        var postfix;
-        var capitals =  parseInt( $lettersCaseSelect.val() );
-        var currentFont = $fontFamilyInput.val();
-
-        capitals ? postfix = 'Capitals' : postfix = '';
-        currentFontData = fonts[currentFont+postfix];
+        $($distanceInput).on('change blur keyup', calculateLettersHeightSimplified);
+        $($symbolsHeight).on('change blur keyup', calculateDistanceSimplified);
 
 
-        //console.log(currentFontData);
+        function calculateLettersHeightSimplified() {
+            var thisValue, lettersHeight;
 
-        if (!currentFontData) currentFontData = defaultFontData;
+            if (bypassCalculationForHeight) return;
 
-        calculateRequiredFontBodySize();
-        calculateDistance();
+            thisValue = $distanceInput.val();
+
+            if (!thisValue) return;
+            if (thisValue == currentDistance) return;
+
+            bypassCalculationForDistance = true;
+            lettersHeight = thisValue * currentFontData.ratio;
+            $symbolsHeight.val( lettersHeight.toFixed(2) );
+            setTimeout(function() { bypassCalculationForDistance = null; }, 100);
+
+            currentDistance = thisValue;
+            currentHeight = lettersHeight;
+        }
+
+        function calculateDistanceSimplified() {
+            var thisValue, distance;
+
+            if (bypassCalculationForDistance) return;
+
+
+
+            thisValue = $symbolsHeight.val();
+            //if (thisValue) thisValue = thisValue.replace(/\D/g,'');
+
+            if (!thisValue) return;
+            if (thisValue == currentHeight) return;
+
+            distance = thisValue / currentFontData.ratio;
+            bypassCalculationForHeight = true;
+            $distanceInput.val( distance.toFixed(2) );
+            setTimeout(function() { bypassCalculationForHeight = null; }, 100);
+
+            currentDistance = distance;
+            currentHeight = thisValue;
+        }
+
+        function initMap() {
+            map = new ymaps.Map("mapus-map", {
+                center: [46.4845,30.7418],
+                zoom: 17
+            });
+
+            map.behaviors.get('ruler').geometry.events.add('change', function(e) {
+                console.warn( map.behaviors.get('ruler').geometry.getDistance() );
+                var distance = map.behaviors.get('ruler').geometry.getDistance();
+                $distanceInput.val(distance.toFixed(2));
+                if (distance) calculateLettersHeightSimplified();
+            });
+
+        }
+
     }
-
-    function calculateRequiredFontBodySize() {
-        var lettersHeight, requiredBodySize;
-
-        lettersHeight = getLettersHeight();
-        $lettersHeightOutput.html( lettersHeight.toFixed(2) );
-
-        requiredBodySize = lettersHeight * currentFontData.m / 10;
-        $lettersBodySizeOutput.html( requiredBodySize.toFixed(2) );
-
-        $symbolsHeight.val( requiredBodySize.toFixed(2) )
-    }
-
-    function calculateDistance() {
-
-        var distance, sybmolsHeight, fontBodySize, fontK, fontM;
-        fontBodySize = $letterBodySizeInput.val() || 0;
-        fontK = currentFontData.k;
-        fontM = currentFontData.m;
-
-        distance = fontBodySize / Math.tan( radians( dotSize * fontK) ) / 100 / fontM;
-        $distanceOutput.html(distance.toFixed(2) );
-
-        sybmolsHeight = fontBodySize / fontM * 10;
-        //$symbolsHeight.html( sybmolsHeight.toFixed(2) );
-
-        $symbolsHeight.val( sybmolsHeight.toFixed(2) );
-
-        console.log(sybmolsHeight.toFixed(2) );
-        //$distanceInput.val( sybmolsHeight.toFixed(2) )
-
-    }
-
-    function getLettersHeight() {
-        var distance, result, fontK;
-
-        fontK = currentFontData.k;
-        distance = $distanceInput.val();
-
-        result = distance * Math.tan( radians(dotSize * fontK) ) * 1000;
-
-        return result;
-    }
-
-    function radians(degrees) {
-        return degrees * (Math.PI / 180);
-    }
-
 
 })(jQuery);
